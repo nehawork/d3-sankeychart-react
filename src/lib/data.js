fd6294b6e3dfa27d8ef64dbcce5a447047e9b2d9
nehawork <neharamchandani.work@gmail.com>
2022-03-05 17:36:46 +0530

    unique key for nested bucket and for display used actual value

--- src/lib/data.js
+++ src/lib/data.js
@@ -1,23 +1,153 @@
+// const jsonDataOld = [
+//   {
+//     key: "a1",
+//     value: 30,
+//     buckets: [
+//       {
+//         key: "a2",
+//         value: 10,
+//         buckets: [{ key: "a3", value: 99 }],
+//       },
+//       { key: "b2", value: 20 },
+//     ],
+//   },
+//   {
+//     key: "b1",
+//     value: 70,
+//     buckets: [
+//       { key: "b2", value: 40 },
+//       { key: "c2", value: 30 },
+//     ],
+//   },
+// ];
+
 const unformattedJson = [
   {
-    key: "a1",
-    value: 30,
     buckets: [
       {
-        key: "a2",
-        value: 10,
-        buckets: [{ key: "a3", value: 99 }],
+        buckets: [
+          {
+            key: "fieldName_2022-02-22T02:54:47.269Z",
+            displayName: "2022-02-22T02:54:47.269Z",
+            value: 1,
+          },
+        ],
+        key: "2022-02-22T02:54:47.269Z",
+        displayName: "2022-02-22T02:54:47.269Z",
+        value: 1,
       },
-      { key: "b2", value: 20 },
+      {
+        buckets: [
+          {
+            key: "fieldName_2022-02-22T02:54:47.269Z",
+            displayName: "2022-02-22T02:54:47.269Z",
+            value: 1,
+          },
+        ],
+        key: "2022-02-22T02:52:24.554Z",
+        displayName: "2022-02-22T02:52:24.554Z",
+        value: 1,
+      },
+      {
+        buckets: [
+          {
+            key: "fieldName_2022-02-22T02:54:47.269Z",
+            displayName: "2022-02-22T02:54:47.269Z",
+            value: 1,
+          },
+        ],
+        key: "2022-02-22T02:49:56.990Z",
+        displayName: "2022-02-22T02:49:56.990Z",
+        value: 1,
+      },
+      {
+        buckets: [
+          {
+            key: "fieldName_2022-02-22T02:54:47.269Z",
+            displayName: "2022-02-22T02:54:47.269Z",
+            value: 1,
+          },
+        ],
+        key: "2022-02-22T02:49:54.855Z",
+        displayName: "2022-02-22T02:49:54.855Z",
+        value: 1,
+      },
+      {
+        buckets: [
+          {
+            key: "fieldName_2022-02-22T02:54:47.269Z",
+            displayName: "2022-02-22T02:54:47.269Z",
+            value: 1,
+          },
+        ],
+        key: "2022-02-22T02:48:44.141Z",
+        displayName: "2022-02-22T02:48:44.141Z",
+        value: 1,
+      },
+      {
+        buckets: [
+          {
+            key: "fieldName_2022-02-22T02:54:47.269Z",
+            displayName: "2022-02-22T02:54:47.269Z",
+            value: 1,
+          },
+        ],
+        key: "2022-02-22T02:48:42.424Z",
+        displayName: "2022-02-22T02:48:42.424Z",
+        value: 1,
+      },
+      {
+        buckets: [
+          {
+            key: "fieldName_2022-02-22T02:54:47.269Z",
+            displayName: "2022-02-22T02:54:47.269Z",
+            value: 1,
+          },
+        ],
+        key: "2022-02-22T02:48:39.005Z",
+        displayName: "2022-02-22T02:48:39.005Z",
+        value: 1,
+      },
+      {
+        buckets: [
+          {
+            key: "fieldName_2022-02-22T02:54:47.269Z",
+            displayName: "2022-02-22T02:54:47.269Z",
+            value: 1,
+          },
+        ],
+        key: "2022-02-22T02:48:36.742Z",
+        displayName: "2022-02-22T02:48:36.742Z",
+        value: 1,
+      },
+      {
+        buckets: [
+          {
+            key: "fieldName_2022-02-22T02:54:47.269Z",
+            displayName: "2022-02-22T02:54:47.269Z",
+            value: 1,
+          },
+        ],
+        key: "2022-02-22T02:47:23.345Z",
+        displayName: "2022-02-22T02:47:23.345Z",
+        value: 1,
+      },
+      {
+        buckets: [
+          {
+            key: "fieldName_2022-02-22T02:54:47.269Z",
+            displayName: "2022-02-22T02:54:47.269Z",
+            value: 1,
+          },
+        ],
+        key: "2022-02-22T02:47:20.756Z",
+        displayName: "2022-02-22T02:47:20.756Z",
+        value: 1,
+      },
     ],
-  },
-  {
-    key: "b1",
-    value: 70,
-    buckets: [
-      { key: "b2", value: 40 },
-      { key: "c2", value: 30 },
-    ],
+    key: "127.0.0.1",
+    value: 34,
+    displayName: "127.0.0.1",
   },
 ];
 
@@ -33,7 +163,7 @@ const getData = (
 
     if (!visited.has(x.key)) {
       let currId = nodes.length;
-      nodes.push({ nodeId: currId, name: x.key });
+      nodes.push({ nodeId: currId, name: x.displayName });
       visited.set(x.key, currId);
     }
 
